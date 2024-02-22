fc125496eef40efc12206684b71f156be9d81cdd
hoanhua14 <huasohoan@gmail.com>
2024-02-21 22:31:34 -0800

    q9 react components that renders a reversed version of a given name

---
+++ q8-9/profile/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Profile from './q8';
+import ReverseTool from './q9';
+
+function App() {
+  return (
+    <div className="App">
+      <Profile/>
+      <ReverseTool name='hoanhua'/>
+    </div>
+  );
+}
+
+export default App;
---
+++ q8-9/profile/src/q9.js
@@ -0,0 +1,19 @@
+
+// Write a method on the component that takes in one argument for name and returns a string with the letters reversed.
+
+import React from 'react';
+
+const ReverseTool = ({name}) => {
+    const reverseName = (name) => {
+            return name.split('').reverse().join('');
+        }
+
+    return (
+        <>
+            {name && (
+                <div>Reversed name: {reverseName(name)}</div>
+            )}
+        </>
+    )
+}
+export default ReverseTool;
