5db6ebc4cbe22027d7c720a1db5cdc4609c11cfd
hoanhua14 <huasohoan@gmail.com>
2024-02-21 22:07:20 -0800

    q8 functional react component that renders a string using props

---
+++ q8/profile/src/q8.js
@@ -0,0 +1,13 @@
+// Write a functional component in React that accepts two props, name and age, and renders a string that displays those two values.
+
+import React from 'react';
+const Profile = ({name, age}) => {
+
+    return (
+        <>
+            <div>Output: {name}{age}</div>
+
+        </>
+    )
+}
+export default Profile;
